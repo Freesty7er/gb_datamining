8695bb129e7c1568e425ca357b1327574c39d668
Freesty7er <ian.anfalov@gmail.com>
2021-06-06 15:09:14 +0300

    добавлена зарплата

--- lesson-02/main.py
+++ lesson-02/main.py
@@ -14,18 +14,24 @@ def RabotaUA(vacancy_name, vacancy_list):
     soup = BeautifulSoup(req.content, 'lxml')
     #print(soup.prettify())
 
-    unprocessed_result = soup.select('h2.card-title a.ga_listing')
+    unprocessed_vacancy = soup.select('h2.card-title a.ga_listing')
+    unprocessed_salary = soup.select('span.salary')
 
-    for i in unprocessed_result:
+
+    for i,value in enumerate(unprocessed_vacancy):
+
+        salary = unprocessed_salary[i].string
+        if salary is None:
+            salary = '0.00  грн'
         vacancy = {
-            'Name' : i['title'],
-            'Link' : site_head + i['href'],
-            'Site' : 'rabota.ua'
+            'Name': value['title'],
+            'Link': site_head + value['href'],
+            'Salary': salary.replace(u'\xa0', u''),
+            'Site': 'rabota.ua'
         }
-        #print(i['href'])
-        #print(i['title'])
 
         vacancy_list.append(vacancy)
+
 
 def GRC(vacancy_name, vacancy_list):
     header = Headers(headers=True).generate()
@@ -36,19 +42,26 @@ def GRC(vacancy_name, vacancy_list):
     soup = BeautifulSoup(req.content, 'lxml')
     #print(soup.prettify())
 
-    unprocessed_result = soup.select('span.g-user-content a.bloko-link')
-    #print(unprocessed_result)
+    unprocessed_vacancy = soup.select('span.g-user-content a.bloko-link')
+    unprocessed_salary = soup.select('div.vacancy-serp-item__sidebar span')
+    #print(unprocessed_salary)
 
-    for i in unprocessed_result:
+    for i, value in enumerate(unprocessed_vacancy):
+
+        salary = unprocessed_salary[i].string
+        if salary is None:
+            salary = '0.00  грн'
+
         vacancy = {
-            'Name' : i.string,
-            'Link' : site_head + i['href'],
-            'Site' : 'grc.ua'
+            'Name': value.string,
+            'Link': site_head + value['href'],
+            'Salary': salary.replace(u'\u202f', u''),
+            'Site': 'grc.ua'
         }
-        #print(i['href'])
-        #print(i['title'])
 
         vacancy_list.append(vacancy)
+
+
 
 vacancy_list = []
 
@@ -56,7 +69,7 @@ RabotaUA('грузчик', vacancy_list)
 
 GRC('gruzchik', vacancy_list)
 
-#print(vacancy_list)
+print(vacancy_list)
 
 with open('vacancy.json', mode='w', encoding='utf-8') as l:
     json.dump(vacancy_list, l, indent=4)
